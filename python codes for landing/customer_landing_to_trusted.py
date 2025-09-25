c0a493fda344259ad3f475de7c868cc03fefabf0
Ayush12r4 <mohantyayush11@gmail.com>
2025-09-25 12:08:56 +0530

    Update customer_landing_to_trusted.py

--- python codes for landing/customer_landing_to_trusted.py
+++ python codes for landing/customer_landing_to_trusted.py
@@ -1,1 +1,46 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+from awsglue import DynamicFrame
 
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node AWS Glue Data Catalog
+AWSGlueDataCatalog_node1758774963728 = glueContext.create_dynamic_frame.from_catalog(database="ayush-project", table_name="customer-landing", transformation_ctx="AWSGlueDataCatalog_node1758774963728")
+
+# Script generated for node SQL Query
+SqlQuery0 = '''
+select * from myDataSource where sharewithresearchasofdate
+is not null;
+
+'''
+SQLQuery_node1758774977192 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":AWSGlueDataCatalog_node1758774963728}, transformation_ctx = "SQLQuery_node1758774977192")
+
+# Script generated for node Amazon S3
+EvaluateDataQuality().process_rows(frame=SQLQuery_node1758774977192, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1758774915256", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AmazonS3_node1758775177224 = glueContext.getSink(path="s3://ayush-project-udacity/customer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1758775177224")
+AmazonS3_node1758775177224.setCatalogInfo(catalogDatabase="ayush-project",catalogTableName="customer-trusted")
+AmazonS3_node1758775177224.setFormat("json")
+AmazonS3_node1758775177224.writeFrame(SQLQuery_node1758774977192)
+job.commit()
